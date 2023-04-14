741f52f070af67990da779948de4b3e23e5c3676
Laura Oliveira <94492629+starklocs@users.noreply.github.com>
2023-04-14 10:36:14 -0300

    Fundamentos de Java
    Fundamentos de Java

---
+++ exerciciosudemyjava/src/fundamentos/operadores/Ternario.java
@@ -0,0 +1,13 @@
+package fundamentos.operadores;
+
+public class Ternario {
+	public static void main(String[] args) {
+
+		double media = 8.6;
+		String resultadoParcial = media >= 5.0 ? "de recuperação." : "Reprovado.";
+		String resultadoFinal = media >= 7.0 ?
+				"Aprovado." : resultadoParcial;
+
+		System.out.println("O aluno está " + resultadoFinal);
+	}
+}
