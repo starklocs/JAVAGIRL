92e2249324371a710c6303753c53a6f9015c54c9
Laura Oliveira <94492629+starklocs@users.noreply.github.com>
2023-04-16 13:38:28 -0300

    Estudos De Pilha e Lista

--- Bruno/src/Listas/Pilha/Pilha.java
+++ Bruno/src/Listas/Pilha/Pilha.java
@@ -1,58 +1,59 @@
 package Listas.Pilha;
 
 public class Pilha {
-	    Integer[] pilha = new Integer[10];
-	    private Integer topo =null;
+	Integer[] pilha = new Integer[10];
+	private Integer topo = null;
 
-	    public boolean isFull(){
-	        if(topo == null){
-	            return false;
-	        }
-	        return topo == pilha.length -1;
-	    }
-	    public boolean isEmpty(){
-	        return topo == null;
-	    }
-	    public  void empilhar(Integer elemento){
-	        if(isFull()){
-	            System.out.println("Pilha está cheia!");
-	            return;
-	        }
-	        if(topo == null){
-	            topo = 0;
-	            pilha[topo]= elemento;
-	        }else{
-	            topo = topo+1;
-	            pilha[topo] = elemento;
-	        }
+	public boolean isFull() {
+		if (topo == null) {
+			return false;
+		}
+		return topo == pilha.length - 1;
+	}
 
-	    }
+	public boolean isEmpty() {
+		return topo == null;
+	}
 
-	    public Integer desempilhar(){
-	        if(isEmpty()){
-	            System.out.println("Pilha está vázia!");
-	            return null;
-	        }
-	        Integer desempilhado = pilha[topo];
-	        pilha[topo] = null;
-
-	        if(topo > 0){
-	            topo =  topo-1;
-	        }else {
-	            topo = null;
-	        }
-	        topo--;
-
-	        System.out.println("Desempilhado: "+desempilhado);
-	        return desempilhado;
-	    }
-
-	    public void imprimirPilha(){
-	        for (int i=0; i < pilha.length; i++){
-	            if(pilha[i] != null){
-	                System.out.println("[ "+pilha[i]+" ]");
-	            }
-	        }
-	    }
+	public void empilhar(Integer elemento) {
+		if (isFull()) {
+			System.out.println("Pilha está cheia!");
+			return;
+		}
+		if (topo == null) {
+			topo = 0;
+			pilha[topo] = elemento;
+		} else {
+			topo = topo + 1;
+			pilha[topo] = elemento;
+		}
 
 	}
+
+	public Integer desempilhar() {
+		if (isEmpty()) {
+			System.out.println("Pilha está vázia!");
+			return null;
+		}
+		Integer desempilhado = pilha[topo];
+		pilha[topo] = null;
+
+		if (topo > 0) {
+			topo = topo - 1;
+		} else {
+			topo = null;
+		}
+
+		System.out.println("Desempilhado: " + desempilhado);
+		return desempilhado;
+	}
+
+	public void imprimirPilha() {
+		for (int i = 0; i < pilha.length; i++) {
+			if (pilha[i] != null) {
+				System.out.println("[ " + pilha[i] + " ]");
+			}
+		}
+	}
+
+}
--- Bruno/src/Listas/Pilha/TestePilha.java
+++ Bruno/src/Listas/Pilha/TestePilha.java
@@ -4,10 +4,24 @@ public class TestePilha {
 	
 	    public static void main(String[] args) {
 	        Pilha pilha = new Pilha();
-	        pilha.empilhar(10);
+	        
+	        pilha.empilhar(1);
+	        pilha.empilhar(2);
+	        pilha.empilhar(3);
+	        pilha.empilhar(4);
+	        pilha.empilhar(5);
+	        pilha.empilhar(6);
 	        pilha.empilhar(7);
-	        pilha.empilhar(90);
+	        pilha.empilhar(8);
+	        pilha.empilhar(9);
+	        pilha.empilhar(10);
+	        
 	        pilha.imprimirPilha();
+	        
+	        System.out.println("\n-----------------------\n");
+	        
+	        pilha.desempilhar();
+	        pilha.desempilhar();
 	        pilha.desempilhar();
 	        pilha.imprimirPilha();
 	    }
