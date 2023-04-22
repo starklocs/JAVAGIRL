90f8031d31d130342104467027c5d76d8f2ca942
Laura Oliveira <94492629+starklocs@users.noreply.github.com>
2023-04-21 23:39:20 -0300

    Pilha e Fila

--- Bruno/src/Listas/Pilha/TestePilha.java
+++ Bruno/src/Listas/Pilha/TestePilha.java
@@ -1,29 +1,29 @@
 package Listas.Pilha;
 
 public class TestePilha {
-	
-	    public static void main(String[] args) {
-	        Pilha pilha = new Pilha();
-	        
-	        pilha.empilhar(1);
-	        pilha.empilhar(2);
-	        pilha.empilhar(3);
-	        pilha.empilhar(4);
-	        pilha.empilhar(5);
-	        pilha.empilhar(6);
-	        pilha.empilhar(7);
-	        pilha.empilhar(8);
-	        pilha.empilhar(9);
-	        pilha.empilhar(10);
-	        
-	        pilha.imprimirPilha();
-	        
-	        System.out.println("\n-----------------------\n");
-	        
-	        pilha.desempilhar();
-	        pilha.desempilhar();
-	        pilha.desempilhar();
-	        pilha.imprimirPilha();
-	    }
-	
+
+	public static void main(String[] args) {
+		Pilha pilha = new Pilha();
+
+		pilha.empilhar(1);
+		pilha.empilhar(2);
+		pilha.empilhar(3);
+		pilha.empilhar(4);
+		pilha.empilhar(5);
+		pilha.empilhar(6);
+		pilha.empilhar(7);
+		pilha.empilhar(8);
+		pilha.empilhar(9);
+		pilha.empilhar(10);
+
+		pilha.imprimirPilha();
+
+		System.out.println("\n-----------------------\n");
+
+		pilha.desempilhar();
+		pilha.desempilhar();
+		pilha.desempilhar();
+		pilha.imprimirPilha();
+	}
+
 }
--- Bruno/src/Listas/testar_listas.java
+++ Bruno/src/Listas/testar_listas.java
@@ -1,24 +1,23 @@
 package Listas;
 
-
 public class testar_listas {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        listaSequencial lista = new listaSequencial();
-        //lista.inserirElemento(20);
-        
-        listaSequencial lista1 = new listaSequencial(); 
-		
+		listaSequencial lista = new listaSequencial();
+		// lista.inserirElemento(20);
+
+		listaSequencial lista1 = new listaSequencial();
+
 		lista.inserirElemento(42);
 		lista.inserirElemento(99);
 		lista.inserirElemento(77);
-        System.out.println("A lista esta cheia "+lista.estaCheia());
-        System.out.println("A lista esta Vazia "+lista.estaVazia());
-        
-        lista.remover(100);
-        lista.apaga(1);
-        lista.inserirElemento(3,7);
-		
-        System.out.println("Elementos "+lista.imprimirLista());
-    }
+		System.out.println("A lista esta cheia " + lista.estaCheia());
+		System.out.println("A lista esta Vazia " + lista.estaVazia());
+
+		lista.remover(100);
+		lista.apaga(0);
+		lista.inserirElemento(3, 7);
+
+		System.out.println("Elementos " + lista.imprimirLista());
+	}
 }
